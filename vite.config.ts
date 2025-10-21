73b3e68a74192012c8a67cffe15eca5649f20857
TANNMANN84 <144026524+TANNMANN84@users.noreply.github.com>
2025-10-21 16:33:13 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 1. Add this line
-  base: '/fractal-edu/',
+ base: '/Fractal-EDU/',
 
   plugins: [react()],
   build: {
